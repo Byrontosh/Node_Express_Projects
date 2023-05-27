c64b6d1302ced3408a29282f2b3d9bbebe2436f4
Byrontosh <by_tosh20@hotmail.com>
2023-05-27 15:51:11 -0500

    API RESTFUL VETERINARY MANAGEMENT

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -21,7 +21,7 @@ const sendMailToUser = async(userMail,token)=>{
     html: `
     <h1>Sistema de gestión (VET-ESFOT 🐶 😺)</h1>
     <hr>
-    <a href="http://localhost:3000/api/confirmar/${token}">Clic para confirmar tu cuenta</a>
+    <a href="http://localhost:5173/confirmar/${token}">Clic para confirmar tu cuenta</a>
     <hr>
     <footer>Grandote te da la Bienvenida!</footer>
     `
@@ -39,7 +39,7 @@ const sendMailToRecoveryPassword = async(userMail,token)=>{
     html: `
     <h1>Sistema de gestión (VET-ESFOT 🐶 😺)</h1>
     <hr>
-    <a href="http://localhost:3000/api/recuperar-password/${token}">Clic para reestablecer tu contraseña</a>
+    <a href="http://localhost:5173/recuperar-password/${token}">Clic para reestablecer tu contraseña</a>
     <hr>
     <footer>Grandote te da la Bienvenida!</footer>
     `
--- src/controllers/paciente_controller.js
+++ src/controllers/paciente_controller.js
@@ -2,7 +2,7 @@ import Paciente from "../models/Paciente.js"
 import mongoose from "mongoose"
 
 const listarPacientes = async (req,res)=>{
-    const pacientes = await Paciente.find({estado:true}).select("-salida -createdAt -updatedAt -__v").populate('veterinario','_id nombre apellido')
+    const pacientes = await Paciente.find({estado:true}).where('veterinario').equals(req.veterinarioBDD).select("-salida -createdAt -updatedAt -__v").populate('veterinario','_id nombre apellido')
     res.status(200).json(pacientes)
 }
 
@@ -10,7 +10,7 @@ const listarPacientes = async (req,res)=>{
 const detallePaciente = async(req,res)=>{
     const {id} = req.params
     if( !mongoose.Types.ObjectId.isValid(id) ) return res.status(404).json({msg:`Lo sentimos, no existe el veterinario ${id}`});
-    const paciente = await Paciente.findById(id).select("-salida -createdAt -updatedAt -__v").populate('veterinario','_id nombre apellido')
+    const paciente = await Paciente.findById(id).select("-createdAt -updatedAt -__v").populate('veterinario','_id nombre apellido')
     res.status(200).json(paciente)
 }
 
@@ -18,7 +18,7 @@ const detallePaciente = async(req,res)=>{
 const registrarPaciente = async(req,res)=>{
     if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Lo sentimos, debes llenar todos los campos"})
     const nuevoPaciente = new Paciente(req.body)
-    nuevoPaciente.veterinario=req.veterinarioBDD._id
+    nuevoPaciente.veterinario=req.body.id
     await nuevoPaciente.save()
     res.status(200).json({msg:"Registro exitoso del paciente"})
 }
